f148f99acfa604e0fa17535f3640b20aba69903c
Sergio <galdos.sergi@outlook.com>
2024-12-16 19:50:17 +0100

    [dev] first comit linear mtion constant speed

---
+++ src/Physics.js
@@ -0,0 +1,11 @@
+
+export default class Physics {
+  constructor(vLimit){
+    // velocidad actual en x
+    this.vx = 0
+    // velocidad actual en y
+    this.vy = 0
+    // velocidad máxima
+    this.vLimit = vLimit
+  }
+}
